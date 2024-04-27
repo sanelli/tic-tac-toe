62a0778b3cf297b41e333e9c3357a2ee62d99ca5
Stefano Anelli <stefano.anelli@gmail.com>
2024-04-27 13:13:06 +0100

    Created (empty) textures

--- src/Rendering.adb
+++ src/Rendering.adb
@@ -1,13 +1,19 @@
-with Ada.Text_IO; use Ada.Text_IO;
 with SDL;
-with SDL.Video.Windows.Makers;
 with SDL.Events;
 with SDL.Events.Events;
+with SDL.Video.Surfaces;
+with SDL.Video.Windows.Makers;
+with SDL.Video.Textures.Makers;
+with SDL.Video.Renderers.Makers;
+with SDL.Video.Surfaces.Makers;
+with Ada.Text_IO; use Ada.Text_IO;
 
 package body Rendering is
 
    function Create (rendering : in out TRendering) return Boolean
    is
+      TmpSurface : SDL.Video.Surfaces.Surface;
+      Sizes      : SDL.Sizes;
    begin
       if not SDL.Initialise then
          Put_Line ("Cannot initialize SDL.");
@@ -17,15 +23,50 @@ package body Rendering is
       SDL.Video.Windows.Makers.Create
          (rendering.Window, "Tic Tac Toe", 10, 10, 300, 300);
 
+      SDL.Video.Renderers.Makers.Create (rendering.Renderer, rendering.Window);
+
+      Sizes.Height := 100;
+      Sizes.Width := 100;
+
+      --  Create empty texture
+      SDL.Video.Surfaces.Makers.Create (TmpSurface, Sizes, 32, 0, 0, 0, 0);
+      
+      SDL.Video.Textures.Makers.Create (
+         rendering.EmptyTexture,
+         rendering.Renderer,
+         TmpSurface);
+      SDL.Video.Surfaces.Finalize (TmpSurface);
+
+      --  Create cross texture
+      SDL.Video.Surfaces.Makers.Create (TmpSurface, Sizes, 32, 0, 0, 0, 0);
+      SDL.Video.Textures.Makers.Create (
+         rendering.CrossTexture,
+         rendering.Renderer,
+         TmpSurface);
+      SDL.Video.Surfaces.Finalize (TmpSurface);
+
+      --  Create circle texture
+      SDL.Video.Surfaces.Makers.Create (TmpSurface, Sizes, 32, 0, 0, 0, 0);
+      SDL.Video.Textures.Makers.Create (
+         rendering.CircleTexture,
+         rendering.Renderer,
+         TmpSurface);
+      SDL.Video.Surfaces.Finalize (TmpSurface);
+
       return True;
    end Create;
 
    procedure Finalise (rendering : in out TRendering)
    is
    begin
+      SDL.Video.Textures.Destroy (rendering.EmptyTexture);
+      SDL.Video.Textures.Destroy (rendering.CircleTexture);
+      SDL.Video.Textures.Destroy (rendering.CrossTexture);
       SDL.Finalise;
    end Finalise;
 
+   -- TODO: MOVE THE PROCESSING EVENT OUTISIDE THE RENDERE
+   -- RENDERR SHOUDLD ONLY EXPOSE TWO METHODS: RenderBoard and RenderWinner
    procedure ProcessEvent
       (rendering : in out TRendering; running : out Boolean)
    is
--- src/Rendering.ads
+++ src/Rendering.ads
@@ -1,12 +1,14 @@
 with SDL.Video.Windows;
 with SDL.Video.Textures;
+with SDL.Video.Renderers;
 
 package Rendering is
 
    type TRendering is record
        Window           : SDL.Video.Windows.Window;
+       Renderer         : SDL.Video.Renderers.Renderer;
        CrossTexture     : SDL.Video.Textures.Texture;
-       CircleTexture    : SDL.Video.Textures.Texture; 
+       CircleTexture    : SDL.Video.Textures.Texture;
        EmptyTexture     : SDL.Video.Textures.Texture;
    end record;
 
